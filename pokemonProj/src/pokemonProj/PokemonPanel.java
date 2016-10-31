a2a0aefb602466deb27fb9f000b52438f2780c25
aliu8 <aliu8@mail.ccsf.edu>
2016-10-31 15:32:11 -0700

    Add files via upload

--- pokemonProj/src/pokemonProj/PokemonPanel.java
+++ pokemonProj/src/pokemonProj/PokemonPanel.java
@@ -22,7 +22,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class PokemonPanel extends JPanel{
+public class PokemonPanel extends JPanel
+{
 	
 	static JFrame frame;
 	
@@ -48,33 +49,41 @@ public class PokemonPanel extends JPanel{
 	Font fontSize = new Font("Courier New", 1, 26);
 	boolean backGround = false;
 	
-	public PokemonPanel(){
+	String filePath = new File("").getAbsolutePath();
+	String directory = filePath.concat("\\pokemon_data.txt");
+	String image_dir = filePath.concat("\\go sprites resized_100x100\\");
+	
+	public PokemonPanel()
+	{
 		
-		
-		
-		String directory = "C:\\Users\\25zsa_000\\Desktop\\Eclipse packages\\pokemonProj\\pokemon_data.txt";
 		Scanner pkmnIn = null;
 		
-		try {
+		try 
+		{
 			pkmnIn = new Scanner(new File(directory));
-		} catch (FileNotFoundException e) {
+		} 
+		catch (FileNotFoundException e) 
+		{
 			System.out.println("Please specify a proper directory");
 			e.printStackTrace();
 		}
 		
 		String[] currentPkmn;
 		int counter = 0;
-		while(pkmnIn.hasNextLine()){
+		while(pkmnIn.hasNextLine())
+		{
 			currentPkmn = pkmnIn.nextLine().split(",");
 			pokeType123[counter] = currentPkmn[1];//stores the pokemon type(s)
 			pokeName123[counter] = currentPkmn[0];//stores the pokemon name in # order
 			pokeNameABC[counter] = currentPkmn[0];//stores the pokemon name in alphabetical order			
 			
 			
-			if(currentPkmn[1].contains("/")){
+			if(currentPkmn[1].contains("/"))
+			{
 				pokemon.put(currentPkmn[0], new Type(currentPkmn[1].split("/")));
 			}
-			else{
+			else
+			{
 				pokemon.put(currentPkmn[0], new Type(currentPkmn[1]));
 			}
 			
@@ -88,14 +97,17 @@ public class PokemonPanel extends JPanel{
 		listType = new JComboBox(reuseType.getTYPES());
 		
 		
-		for(int i = 0; i < NUMOFPOKEMON; i++){
+		for(int i = 0; i < NUMOFPOKEMON; i++)
+		{
 			pokeNameLabel[i] = new JLabel(pokeName123[i]);
 		}// assign each label a Pokemon name in 123 order.
 		
 		
 		//NEED PICTURE FILES
-		for(int x = 0; x < NUMOFPOKEMON; x++){
-			picLabel[x] = new JLabel(new ImageIcon("C:\\Users\\25zsa_000\\Desktop\\Eclipse packages\\pokemonProj\\go sprites resized_100x100\\" + pokeName123[x] + ".png"));
+		for(int x = 0; x < NUMOFPOKEMON; x++)
+		{
+			picLabel[x] = new JLabel(new ImageIcon(image_dir + pokeName123[x] + ".png"));
+			picLabel[x].setToolTipText("Type:"+pokemon.get(pokeName123[x]));
 		}//attach jpg or png file on to Icon picture, then place all the pictures on to a JLabel
 		
 
@@ -128,51 +140,60 @@ public class PokemonPanel extends JPanel{
 	        
 	}
 	
-	public void paintComponent(Graphics background){
+	public void paintComponent(Graphics background)
+	{
 		super.paintComponent(background);	
-		if(backGround == true){
+		if(backGround == true)
+		{
 			background.setColor(Color.CYAN);
 			background.fillRect(170, 100, 1050, 580);
 		}
 		
-		else{
-		background.setColor(Color.blue);
-		background.drawLine(50, 50, 1000, 700);
-		background.drawOval(500, 500, 100, 100);
-		background.fillOval(300, 300, 200, 200);
-		background.fillRect(100, 100, 2001, 200);
-
-		//do not insert background after THIS line.
-		background.setColor(Color.green);
-		background.setFont(fontSize);
-		background.drawString("Effective Against", 130, 50);
-		background.fillRect(15, 80, 500, 130 + SCROLL * 140);	
-		background.setColor(Color.red);
-		background.drawString("Ineffective Against", 1040, 50);
-		background.fillRect(946, 80, 500, 130 + SCROLL * 140);
+		else
+		{
+			background.setColor(Color.blue);
+			background.drawLine(50, 50, 1000, 700);
+			background.drawOval(500, 500, 100, 100);
+			background.fillOval(300, 300, 200, 200);
+			background.fillRect(100, 100, 2001, 200);
+	
+			//do not insert background after THIS line.
+			background.setColor(Color.green);
+			background.setFont(fontSize);
+			background.drawString("Effective Against", 130, 50);
+			background.fillRect(15, 80, 500, 130 + SCROLL * 140);	
+			background.setColor(Color.red);
+			background.drawString("Ineffective Against", 1040, 50);
+			background.fillRect(946, 80, 500, 130 + SCROLL * 140);
 		}
 //		repaint();
 	}
 
-	private void setSCROLL(int _x){
+	private void setSCROLL(int _x)
+	{
 		SCROLL = _x;
 	}//seems like direct changes made to SCROLL in the ActionListener doesn't permeate to the paintComponent.
 	
-	private void setBackGround(boolean x){
+	private void setBackGround(boolean x)
+	{
 		backGround = x;
 	}
 
-	public class Plistener implements ActionListener{
-		public void actionPerformed(ActionEvent e){
+	public class Plistener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
 			String s = list.getSelectedItem().toString();//takes the string entry of the JComboBox
 			
-			for(int p = 0; p < NUMOFPOKEMON; p++){
+			for(int p = 0; p < NUMOFPOKEMON; p++)
+			{
 				remove(pokeNameLabel[p]);//always reset and remove label when starting new action.
 				remove(picLabel[p]);//always reset and remove the picture when starting a new action.
 			}
 			
 
-			for(int i = 0; i < NUMOFPOKEMON; i++){
+			for(int i = 0; i < NUMOFPOKEMON; i++)
+			{
 				stats[i] = pokemon.get(s).compareTo(pokemon.get(pokeName123[i]));
 			}//assigned every compared pokemon a number -3<0<3
 			
@@ -181,16 +202,19 @@ public class PokemonPanel extends JPanel{
 			String x = pokemon.get(s).toString();//out puts the attributes of the selected pokemon
 			selectedPokemon.setText("Pokemon:	 " + s);
 			selectedType.setText("Type: 	" + x);
-			selectedPicture.setIcon(new ImageIcon("C:\\Users\\25zsa_000\\Desktop\\Eclipse packages\\pokemonProj\\go sprites resized_100x100\\" + s + ".png"));
+			selectedPicture.setIcon(new ImageIcon(image_dir + s + ".png"));
 	
 			int j = 0;
 			int k = 0;
-			for(int i = 0; i < NUMOFPOKEMON; i++){
-				if(j % 5 == 0 && j !=0){
+			for(int i = 0; i < NUMOFPOKEMON; i++)
+			{
+				if(j % 5 == 0 && j !=0)
+				{
 					j = 0;
 					k++;
 				}//go to the next row and start from the most left column
-				if(stats[i] > 0){
+				if(stats[i] > 0)
+				{
 					add(pokeNameLabel[i]);
 					add(picLabel[i]);
 					pokeNameLabel[i].setBounds(20 + 100 * j, 200 + 130 * k, SIZE, 20);
@@ -202,12 +226,15 @@ public class PokemonPanel extends JPanel{
 			
 			int j2 = 0;
 			int k2 = 0;
-			for(int i = 0; i < NUMOFPOKEMON; i++){
-				if(j2 % 5 == 0 && j2 !=0){
+			for(int i = 0; i < NUMOFPOKEMON; i++)
+			{
+				if(j2 % 5 == 0 && j2 !=0)
+				{
 					j2 = 0;
 					k2++;
 				}//go to the next row and start from the most left column
-				if(stats[i] < 0){
+				if(stats[i] < 0)
+				{
 					add(pokeNameLabel[i]);
 					add(picLabel[i]);
 					pokeNameLabel[i].setBounds(950 + 100 * j2, 200 + 130 * k2, SIZE, 20);
@@ -219,7 +246,8 @@ public class PokemonPanel extends JPanel{
 			
 			
 			int SCROLL;//sets up the magnitude of the scroll bar
-			if(k > k2){
+			if(k > k2)
+			{
 				SCROLL = k;
 			}
 			else
@@ -233,19 +261,23 @@ public class PokemonPanel extends JPanel{
 	}
 	
 
-	public class MissingNoListener implements ActionListener{
-		public void actionPerformed(ActionEvent e){
+	public class MissingNoListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
 			String s1 = surf.getSelectedItem().toString();
 			if(s1 == "Surf Up")up++;
 			if(s1 == "Surf Down")down++;
-			if(up > 10 && down  > 10){
+			if(up > 10 && down  > 10)
+			{
 				remove(list);
 				remove(surf);
 				remove(listType);
 				selectedPokemon.setText("MissingNo: ???");
 				selectedType.setText("Type: Normal,Bird");
-				selectedPicture.setIcon(new ImageIcon("C:\\Users\\25zsa_000\\Desktop\\Eclipse packages\\pokemonProj\\go sprites resized_100x100\\MissingNo.png"));
-				for(int p = 0; p < NUMOFPOKEMON; p++){
+				selectedPicture.setIcon(new ImageIcon(image_dir+"MissingNo.png"));
+				for(int p = 0; p < NUMOFPOKEMON; p++)
+				{
 					remove(pokeNameLabel[p]);
 					remove(picLabel[p]);
 				}
@@ -255,31 +287,38 @@ public class PokemonPanel extends JPanel{
 	}
 
 	public class TypeListener implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed(ActionEvent e) 
+		{
 			String s2 = listType.getSelectedItem().toString();
-			if(selectedPokemon.getText().isEmpty()){
+			if(selectedPokemon.getText().isEmpty())
+			{
 				
 			}
-			else{
+			else
+			{
 				selectedPokemon.setText("");//isEmpty will nullpointer if setText becomes null;
 				selectedType.setText(null);
 				selectedPicture.setIcon(null);
 			}
 				
 			
-			for(int p = 0; p < NUMOFPOKEMON; p++){
+			for(int p = 0; p < NUMOFPOKEMON; p++)
+			{
 				remove(pokeNameLabel[p]);//always reset and remove label when starting new action.
 				remove(picLabel[p]);//always reset and remove the picture when starting a new action.
 			}
 		
 			int j = 0;
 			int k = 0;
-			for(int i = 0; i < NUMOFPOKEMON; i++){
-				if(j % 10 == 0 && j !=0){
+			for(int i = 0; i < NUMOFPOKEMON; i++)
+			{
+				if(j % 10 == 0 && j !=0)
+				{
 					j = 0;
 					k++;
 				}//go to the next row and start from the most left column
-				if(pokeType123[i].contains(s2)){
+				if(pokeType123[i].contains(s2))
+				{
 					add(pokeNameLabel[i]);
 					add(picLabel[i]);
 					pokeNameLabel[i].setBounds(200 + 100 * j, 200 + 130 * k, SIZE, 20);
